6168ab87c0ac3df2b725441ad120b35a362e075b
maggiecrow <mcrow@cshl.edu>
2015-11-17 16:43:30 -0500

    fig3

--- fig3.r
+++ fig3.r
@@ -49,3 +49,24 @@ write.table(nd_exp,file=paste(file_ext,"nd_vs_exp.txt",sep="."),sep="\t")
 write.table(assort_mat,file=paste(file_ext,"assortativity.txt",sep="."),sep="\t")
 
 }
+
+
+### HELPER FUNCTION -- assort.r ###
+
+assortativity <- function(network){
+
+       diag(network) = 0
+       node_degree=rowSums(network)}
+        
+	# Weighted network
+        	indices = which(!is.na(network), arr.ind=T )
+                w = network[indices]/ sum(network[indices])
+		x =  node_degree[indices[,1]] - sum( node_degree[indices[,1]] * w )
+		y =  node_degree[indices[,2]] - sum( node_degree[indices[,2]] * w )
+		vx = sum( w * x * x )
+		vy = sum( w * y * y )
+		vxy = sum(y * x * w)
+                assort = vxy / sqrt(vx * vy)
+
+	return(assort) 
+}

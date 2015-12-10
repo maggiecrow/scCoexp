502c2cd2dfab1d42a46702a50d5e251c6094b8bf
maggiecrow <mcrow@cshl.edu>
2015-12-10 10:07:45 -0500

    Update fig5.r

--- fig5.r
+++ fig5.r
@@ -193,6 +193,20 @@ fisher.method <- function(pvals, method=c("fisher"), p.corr=c("bonferroni","BH",
   return(fisher.sums)
 }
 
+## function to calculate fisher sum
+## p: vector of p-values
+fisher.sum <- function(p, zero.sub=0.00001, na.rm=FALSE){
+  if(any(p>1, na.rm=TRUE)||any(p<0, na.rm=TRUE))
+    stop("You provided bad p-values")
+  stopifnot(zero.sub>=0 & zero.sub<=1 || length(zero.sub)!=1)
+  p[p==0] <- zero.sub
+  if(na.rm)
+    p<- p[!is.na(p)]
+  S= -2*sum(log(p))
+  res <- data.frame(S=S, num.p=length(p))
+  return(res)
+}
+
 ### get meta-analytic BH adjusted p-values
 metaP_pairwise<-function(pvals) {
   metaP=matrix(0,ncol=1,nrow=dim(pvals)[1])
